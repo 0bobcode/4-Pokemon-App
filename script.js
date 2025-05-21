f7003380506b40ebb7c0b0e14a8f85d92bc36fc0
0bobcode <bobchopracoding@gmail.com>
2025-05-21 11:48:45 +0530

    modify the script to gain more visibility and clarity in terms of response status like: 200:sucseesfull, 404: not found, 429: too many requests, 400: bad request, 500-600: server error.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function fetchPokemonInfo() {
     const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
+    //grab elements to wich we will display result
     const pokemonNameDisplay = document.getElementById('pokemonNameDisplay');
     const pokemonTypes = document.getElementById('pokemonTypes');
     const pokemonAbilities = document.getElementById('pokemonAbilities');
     const pokemonDetails = document.getElementById('pokemonDetails');
 
+    //validate input 
     if (!pokemonName) {
+        console.warn('NO POKEMON NAME ENTERED')
         alert("Please enter a Pokémon name!");
         return;
     }
@@ -14,10 +17,33 @@ function fetchPokemonInfo() {
 
     fetch(url)
         .then(response => {
-            if (!response.ok) {
+            //log the raw httpp status(CODE)
+            console.log('HTTP status code:', response.status)
+            //demonstrate handaling of VARIOUS status codes
+            if (response.status===200) {
+                console.log('Pokémon found HTTP status code:', response.status)
                 throw new Error('Pokémon not found!');
             }
-            return response.json();
+            else if(response.status===404){
+                console.error("404 not found and you can go now")
+                throw new Error('Pokémon not found(404)')
+                
+            }
+
+            else if(response.status===429){
+                console.error("429 uh... excuse(to many requests)")
+                throw new Error('rate limit exceeded(429) try again LATER')
+            }
+
+            else if(response.status>=500&& response.status < 600){
+                console.error(`response status: ${response.status} server error`)
+                throw new Error(`response status: ${response.status} server error please try again`)
+            }
+
+            else{
+                console.error(`(${response.status}) unexpected status`)
+            }
+            
         })
         .then(data => {
             
